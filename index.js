31cc58c1aa9d58eadfdb563e23068457092e9f81
Luca <lukkino31@gmail.com>
2022-11-25 10:12:26 +0100

    install path

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const multer = require('multer');
 const port = process.env.PORT || 5000;
 
+
 app.post('/upload-profile-pic', (req, res) => {
     res.send(`<h1>Page</h1>`)
 })
