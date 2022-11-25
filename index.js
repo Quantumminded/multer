ef23c2598a318b315dbf1ea0f4bbe5aa23c2c50d
Luca <lukkino31@gmail.com>
2022-11-25 10:20:54 +0100

    set 4 done

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 const port = process.env.PORT || 5000;
+const path = require('path');
+
+
+const storage = multer.diskStorage({
+    destination: './uploads',
+    filname: (req,file,cb) => {
+        const extension = path.extname(file.originalname);
+        cb(null, `${file.fieldname}-${Date.now()}${extension}`);
+    }
+});
+
+const upload = multer({storage});
 
 
 app.post('/upload-profile-pic', (req, res) => {
